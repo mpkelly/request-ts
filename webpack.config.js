98df2b87fa8487234d2fd6fda8bfd6136e2dcb00
Michael Kelly <>
2019-03-01 15:37:10 +0700

    remove source map from webpack script

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,6 @@ module.exports = {
     umdNamedDefine: true     
   },
 
-  devtool: "source-map",
-
   resolve: {
     extensions: [".ts"]
   },
@@ -19,7 +17,6 @@ module.exports = {
   module: {
     rules: [
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
 };
